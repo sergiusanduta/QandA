072b49a56425817ecf29a9442bd8e6066f1dd897
Sergiu Sanduta <sergiusanduta@yahoo.com>
2020-09-17 12:47:26 -0500

    signinform

---
+++ QandA/Models/SignInModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace QandA.Models
+{
+    public class SignInModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Please enter the Username")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Please enter a Password")]
+        public string Password { get; set; }
+    }
+}
