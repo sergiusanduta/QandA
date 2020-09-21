89252a1804c970dd61fec8d8a6fe1c5552d3393e
Sergiu Sanduta <sergiusanduta@yahoo.com>
2020-09-21 10:09:04 -0500

    Bootstrap does not work

--- QandA/Models/RegisterModel.cs
+++ QandA/Models/RegisterModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using QandA.Data;
 using System.Linq;
 using System.Security.Permissions;
 using System.Threading.Tasks;
@@ -11,13 +12,13 @@ namespace QandA.Models
     public class RegisterModel
     {
         [Required]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Username may not contain special characters")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username length can't be less than 3 and greater than 20 characters")]
         public string Username { get; set; }
         [Required]
         [StringLength(20, MinimumLength = 5, ErrorMessage = "'Password' and 'Confirm Password' does not match")]
         public string Password  { get; set; }
         [Required]
-        [Compare ("ConfirmPassword")]
+        [Compare ("Password",ErrorMessage ="The 'Password' and confirm password field does not match")]
         [NotMapped]
         [StringLength(20, MinimumLength = 5, ErrorMessage = "Please confirm your Password")]
         public string ConfirmPassword { get; set; }
